9df194ab40ea2e38ecf939fbecd1602e6758cd76
Samuel Desmond Abbey Darko <dehydrus223@gmail.com>
2024-04-27 14:58:47 +0000

    update

--- kkp_luggage/settings.py
+++ kkp_luggage/settings.py
@@ -11,10 +11,13 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 from pathlib import Path
-
+import dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -81,9 +84,13 @@ WSGI_APPLICATION = 'kkp_luggage.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+   'default':{
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'defaultdb',
+        'USER': 'avnadmin',
+        'PASSWORD': 'AVNS_nc0hWulshBuyKnvVDoj',
+        'HOST': 'giftos-sdabbey.a.aivencloud.com',
+        'PORT': '24545',
     }
 }
 
