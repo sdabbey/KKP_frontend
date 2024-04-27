acdf9e12c8cb95940d9302caa60721cced302b77
Samuel Desmond Abbey Darko <dehydrus223@gmail.com>
2024-04-27 03:43:59 +0000

    Added Phonenumber field

--- main/models.py
+++ main/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
 from django.utils import timezone
+from phonenumber_field.modelfields import PhoneNumberField
+
 
 class CustomUserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
@@ -32,3 +34,9 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.email
+    
+
+class Booking(models.Model):
+    full_name = models.CharField(max_length=200)
+    index_number = models.IntegerField(max_length=8)
+    phone_number = PhoneNumberField()
