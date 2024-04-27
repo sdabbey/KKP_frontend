c12f4f643399eb695fa4ea24fba2ca5a0bdbce13
Samuel Desmond Abbey Darko <dehydrus223@gmail.com>
2024-04-27 03:55:21 +0000

    Added Booking model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,4 @@
+import random, string
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
 from django.utils import timezone
@@ -39,4 +40,37 @@ class User(AbstractBaseUser, PermissionsMixin):
 class Booking(models.Model):
     full_name = models.CharField(max_length=200)
     index_number = models.IntegerField(max_length=8)
-    phone_number = PhoneNumberField()
+    phone_number = PhoneNumberField(null=False, blank=False)
+    opt_phone_number = PhoneNumberField(null=True, blank=True)
+    location = models.CharField(max_length=150)
+    luggage_number = models.IntegerField()
+    booking_code = models.CharField(max_length=50, unique=True)
+    booking_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Booking {self.pk}'
+    
+    @staticmethod
+    def generate_booking_code(full_name, phone_number):
+        # Concatenate parts of full_name and phone_number to generate a base code
+        base_code = full_name[:3].upper() + str(phone_number)[-4:]
+
+        # Generate a random suffix to ensure uniqueness
+        suffix = ''.join(random.choices(string.ascii_uppercase + string.digits, k=6))
+
+        # Concatenate base_code and suffix to get the final booking code
+        booking_code = base_code + suffix
+
+        # Check if the generated code already exists in the database
+        while Booking.objects.filter(booking_code=booking_code).exists():
+            # If it does, generate a new suffix and try again
+            suffix = ''.join(random.choices(string.ascii_uppercase + string.digits, k=6))
+            booking_code = base_code + suffix
+
+        return booking_code
+
+    def save(self, *args, **kwargs):
+        # Generate a unique booking code before saving the object
+        if not self.booking_code:
+            self.booking_code = self.generate_booking_code(self.full_name, self.phone_number)
+        super().save(*args, **kwargs)
