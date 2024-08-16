66d38b5344e856a3c8e1f9f1836daca7b190ab4b
sdabbey <dehydrus223@gmail.com>
2024-08-16 00:07:25 +0000

    update

--- kkp_luggage/settings.py
+++ kkp_luggage/settings.py
@@ -91,7 +91,7 @@ DATABASES = {
 
 }
 
-DATABASES['default'] = dj_database_url.config()
+DATABASES['default'] = dj_database_url.config(default=os.getenv('DATABASE_URL'))
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
