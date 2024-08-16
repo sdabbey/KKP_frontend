0f3026349367431a07011e30153c4792b4036966
sdabbey <dehydrus223@gmail.com>
2024-08-16 02:15:52 +0000

    update

--- kkp_luggage/views.py
+++ kkp_luggage/views.py
@@ -3,10 +3,10 @@ from django.shortcuts import render, redirect
 
 def create_superuser(request):
     password = "testing321"
-    if User.objects.filter(email='admin@kkp_luggage.com').exists():
+    if User.objects.filter(email='admin@kkp.com').exists():
         return redirect("bookingpage")
-    User.objects.create_superuser(email='admin@kkp_luggage.com', password=password)
-    user = User.objects.get(email='admin@kkp_luggage.com')
+    User.objects.create_superuser(email='admin@kkp.com', password=password)
+    user = User.objects.get(email='admin@kkp.com')
     user.set_password(password)
     user.save()
     return redirect("bookingpage")
