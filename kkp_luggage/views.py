e2356f44a9c6bafe4e3c48a0ea62ba9e88aa56ac
sdabbey <dehydrus223@gmail.com>
2024-08-16 02:04:45 +0000

    update

--- kkp_luggage/views.py
+++ kkp_luggage/views.py
@@ -5,7 +5,7 @@ def create_superuser(request):
     password = "testing321"
     if User.objects.filter(email='admin@kkp_luggage.com').exists():
         return redirect("bookingpage")
-    User.objects.create_superuser(username="admin", email='admin@kkp_luggage.com', password=password)
+    User.objects.create_superuser(email='admin@kkp_luggage.com', password=password)
     user = User.objects.get(email='admin@kkp_luggage.com')
     user.set_password(password)
     user.save()
